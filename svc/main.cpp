4eff486787d7f5966c5047ac6a621b8722a8228b
tsiguenz <tsiguenz@student.42.fr>
2026-01-01 19:49:47 +0100

    add start and stop and refactoring

--- svc/main.cpp
+++ svc/main.cpp
@@ -1,148 +1,39 @@
-#ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0A00 // Windows 10 minimum
-#endif
+#include "svc.h"
 
-#define WIN32_LEAN_AND_MEAN // reduce size of the windows headers
-#define NOMINMAX
-#define UNICODE
-#define _UNICODE
-#include <iostream>
-#include <map>
-#include <windows.h>
-#pragma comment(lib, "Advapi32.lib")
-
-void Usage(void) {
-  std::cerr << "Usage: svc.exe [install|start|stop|delete]" << std::endl;
+static void Usage(void)
+{
+    std::cerr << "Usage: svc.exe [install|start|stop|delete]" << std::endl;
 }
 
-void cleanup(SC_HANDLE scm, SC_HANDLE serviceHandle) {
-  if (scm) {
-    CloseServiceHandle(scm);
-    scm = nullptr;
-  }
-  if (serviceHandle) {
-    CloseServiceHandle(serviceHandle);
-    serviceHandle = nullptr;
-  }
-}
-
-int ActionInstall(void) {
-  SC_HANDLE scm = nullptr;
-  SC_HANDLE serviceHandle = nullptr;
-
-  scm = OpenSCManager(nullptr, nullptr,
-                      SC_MANAGER_CONNECT | SC_MANAGER_CREATE_SERVICE);
-  if (scm == nullptr) {
-    std::cerr << "Error while opening SCM: " << GetLastError() << std::endl;
-    return 1;
-  }
-
-  LPCWSTR serviceName = L"tinky";
-  LPCWSTR serviceBinaryPath = L"calc.exe";
-
-  // Install the service into SCM by calling CreateService
-  serviceHandle = CreateService(scm,                       // SCManager database
-                                serviceName,               // Name of service
-                                serviceName,               // Name to display
-                                SERVICE_ALL_ACCESS,        // Desired access
-                                SERVICE_WIN32_OWN_PROCESS, // Service type
-                                SERVICE_DEMAND_START,      // Service start type
-                                SERVICE_ERROR_NORMAL,      // Error control type
-                                serviceBinaryPath,         // Service's binary
-                                nullptr, // No load ordering group
-                                nullptr, // No tag identifier
-                                nullptr, // Dependencies
-                                nullptr, // Service running account
-                                nullptr  // Password of the account
-  );
-  if (serviceHandle == nullptr) {
-    DWORD errorCode = GetLastError();
-    int returnValue;
-    if (errorCode == ERROR_SERVICE_EXISTS) {
-      std::cout << "Service is already installed!\n";
-      returnValue = 0;
-    } else {
-      std::cerr << "Error while creating the service: " << errorCode
-                << std::endl;
-      returnValue = 1;
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        std::cerr << "Bad number of arguments.\n";
+        Usage();
+        return 1;
     }
-    cleanup(scm, serviceHandle);
-    return returnValue;
-  }
-  cleanup(scm, serviceHandle);
-  std::cout << "Service installed successfully!\n";
-  return 0;
-}
 
-int ActionStart(void) {
-  std::cout << "Starting...\n";
-  return 1;
-}
+    const std::map<const std::string, int (*)(void)> actions {
+        { "install", &ActionInstall },
+        { "start", &ActionStart },
+        { "stop", &ActionStop },
+        { "delete", &ActionDelete }
+    };
 
-int ActionStop(void) {
-  std::cout << "Stopping...\n";
-  return 0;
-}
-
-int ActionDelete(void) {
-  SC_HANDLE scm = nullptr;
-  SC_HANDLE serviceHandle = nullptr;
-  int returnValue = 0;
-
-  scm = OpenSCManager(nullptr, nullptr, SC_MANAGER_ALL_ACCESS);
-  if (scm == nullptr) {
-    std::cerr << "Error while opening SCM: " << GetLastError() << std::endl;
-    return 1;
-  }
-  serviceHandle = OpenService(scm, L"tinky", DELETE);
-  if (serviceHandle == nullptr) {
-    DWORD errorCode = GetLastError();
-    if (errorCode == ERROR_SERVICE_DOES_NOT_EXIST) {
-      std::cout << "Service does not exist!\n";
-    } else {
-      std::cerr << "Error while opening the service: " << errorCode
-                << std::endl;
-      returnValue = 1;
+    try {
+        int res = actions.at(argv[1])();
+        if (res != 0) {
+            std::cerr << "Error status: " << res << std::endl;
+            return res;
+        }
+    } catch (std::exception& e) {
+        (void)e;
+        std::cerr << "Unknown action: " << argv[1] << std::endl;
+        Usage();
+        return 1;
     }
-    cleanup(scm, serviceHandle);
-    return returnValue;
-  }
-  if (!DeleteService(serviceHandle)) {
-    std::cerr << "Fail to delete service\n";
-    returnValue = 1;
-  } else
-    std::cout << "Service deleted successfully!\n";
-  cleanup(scm, serviceHandle);
-  return returnValue;
-}
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "Bad number of arguments.\n";
-    Usage();
-    return 1;
-  }
+    std::cout << "Done!\n";
 
-  const std::map<const std::string, int (*)(void)> actions{
-      {"install", &ActionInstall},
-      {"start", &ActionStart},
-      {"stop", &ActionStop},
-      {"delete", &ActionDelete}};
-
-  try {
-    int res = actions.at(argv[1])();
-    if (res != 0) {
-      std::cerr << "Error status: " << res << std::endl;
-      return res;
-    }
-  } catch (std::exception &e) {
-    (void)e;
-    std::cerr << "Unknown action: " << argv[1] << std::endl;
-    Usage();
-    return 1;
-  }
-
-  std::cout << "Done!\n";
-
-  return 0;
+    return 0;
 }
---
+++ svc/start.cpp
@@ -0,0 +1,176 @@
+#include "svc.h"
+
+int ActionStart(void)
+{
+    SC_HANDLE scm = nullptr;
+    SC_HANDLE serviceHandle = nullptr;
+    SERVICE_STATUS_PROCESS ssStatus;
+    DWORD dwOldCheckPoint;
+    DWORD dwStartTickCount;
+    DWORD dwWaitTime;
+    DWORD dwBytesNeeded;
+    int returnValue = 0;
+
+    scm = OpenSCManager(nullptr, nullptr, SC_MANAGER_ALL_ACCESS);
+    if (scm == nullptr) {
+        std::cerr << "Error while opening SCM: " << GetLastError() << std::endl;
+        return 1;
+    }
+    serviceHandle = OpenService(scm, L"tinky", SERVICE_ALL_ACCESS);
+    if (serviceHandle == nullptr) {
+        DWORD errorCode = GetLastError();
+        if (errorCode == ERROR_SERVICE_DOES_NOT_EXIST) {
+            std::cout << "Service does not exist!\n";
+        } else {
+            std::cerr << "Error while opening the service: " << errorCode
+                      << std::endl;
+            returnValue = 1;
+        }
+        Cleanup(scm, serviceHandle);
+        return returnValue;
+    }
+
+    // Make sure the service is not already started.
+
+    if (!QueryServiceStatusEx(scm, SC_STATUS_PROCESS_INFO, (LPBYTE)&ssStatus,
+            sizeof(SERVICE_STATUS_PROCESS), &dwBytesNeeded)) {
+        std::cerr << "QueryServiceStatusEx failed: " << GetLastError() << std::endl;
+        Cleanup(scm, serviceHandle);
+        return 1;
+    }
+
+    // Check if the service is already running.
+
+    if (ssStatus.dwCurrentState != SERVICE_STOPPED && ssStatus.dwCurrentState != SERVICE_STOP_PENDING) {
+        std::cout << "Cannot start the service because it is already running\n";
+        Cleanup(scm, serviceHandle);
+        return 0;
+    }
+
+    // Wait for the service to stop before attempting to start it.
+
+    dwStartTickCount = GetTickCount();
+    dwOldCheckPoint = ssStatus.dwCheckPoint;
+
+    while (ssStatus.dwCurrentState == SERVICE_STOP_PENDING) {
+        // Do not wait longer than the wait hint. A good interval is
+        // one-tenth of the wait hint but not less than 1 second
+        // and not more than 10 seconds.
+
+        dwWaitTime = ssStatus.dwWaitHint / 10;
+
+        if (dwWaitTime < 1000)
+            dwWaitTime = 1000;
+        else if (dwWaitTime > 10000)
+            dwWaitTime = 10000;
+
+        Sleep(dwWaitTime);
+
+        // Check the status until the service is no longer stop pending.
+
+        if (!QueryServiceStatusEx(
+                scm, // handle to service
+                SC_STATUS_PROCESS_INFO, // information level
+                (LPBYTE)&ssStatus, // address of structure
+                sizeof(SERVICE_STATUS_PROCESS), // size of structure
+                &dwBytesNeeded)) // size needed if buffer is too small
+        {
+            std::cerr << "QueryServiceStatusEx failed: " << GetLastError() << std::endl;
+            Cleanup(scm, serviceHandle);
+            return 1;
+        }
+
+        if (ssStatus.dwCheckPoint > dwOldCheckPoint) {
+            // Continue to wait and check.
+            dwStartTickCount = GetTickCount();
+            dwOldCheckPoint = ssStatus.dwCheckPoint;
+        } else {
+            if (GetTickCount() - dwStartTickCount > ssStatus.dwWaitHint) {
+                printf("Timeout waiting for service to stop\n");
+                Cleanup(scm, serviceHandle);
+                return 1;
+            }
+        }
+    }
+
+    // Attempt to start the service.
+
+    if (!StartService(serviceHandle, 0, nullptr)) {
+        std::cerr << "Fail to start the service: " << GetLastError() << std::endl;
+        returnValue = 1;
+    } else
+        std::cout << "Service start pending...\n";
+
+    // Check the status until the service is no longer start pending.
+
+    if (!QueryServiceStatusEx(
+            scm, // handle to service
+            SC_STATUS_PROCESS_INFO, // info level
+            (LPBYTE)&ssStatus, // address of structure
+            sizeof(SERVICE_STATUS_PROCESS), // size of structure
+            &dwBytesNeeded)) // if buffer too small
+    {
+        std::cerr << "QueryServiceStatusEx failed: " << GetLastError() << std::endl;
+        Cleanup(scm, serviceHandle);
+        return 1;
+    }
+
+    // Save the tick count and initial checkpoint.
+
+    dwStartTickCount = GetTickCount();
+    dwOldCheckPoint = ssStatus.dwCheckPoint;
+
+    while (ssStatus.dwCurrentState == SERVICE_START_PENDING) {
+        // Do not wait longer than the wait hint. A good interval is
+        // one-tenth the wait hint, but no less than 1 second and no
+        // more than 10 seconds.
+        std::cout << "Service start pending...\n";
+        dwWaitTime = ssStatus.dwWaitHint / 10;
+
+        if (dwWaitTime < 1000)
+            dwWaitTime = 1000;
+        else if (dwWaitTime > 10000)
+            dwWaitTime = 10000;
+
+        Sleep(dwWaitTime);
+
+        // Check the status again.
+
+        if (!QueryServiceStatusEx(
+                scm, // handle to service
+                SC_STATUS_PROCESS_INFO, // info level
+                (LPBYTE)&ssStatus, // address of structure
+                sizeof(SERVICE_STATUS_PROCESS), // size of structure
+                &dwBytesNeeded)) // if buffer too small
+        {
+            std::cerr << "QueryServiceStatusEx failed: " << GetLastError() << std::endl;
+            break;
+        }
+
+        if (ssStatus.dwCheckPoint > dwOldCheckPoint) {
+            // Continue to wait and check.
+
+            dwStartTickCount = GetTickCount();
+            dwOldCheckPoint = ssStatus.dwCheckPoint;
+        } else {
+            if (GetTickCount() - dwStartTickCount > ssStatus.dwWaitHint) {
+                // No progress made within the wait hint.
+                break;
+            }
+        }
+    }
+
+    // Determine whether the service is running.
+
+    if (ssStatus.dwCurrentState == SERVICE_RUNNING) {
+        std::cout << "Service started successfully.\n";
+    } else {
+        std::cout << "Service not started. \n";
+        std::cout << "  Current State: " << ssStatus.dwCurrentState << std::endl;
+        std::cout << "  Exit Code: " << ssStatus.dwWin32ExitCode << std::endl;
+        std::cout << "  Check Point: " << ssStatus.dwCheckPoint << std::endl;
+        std::cout << "  Wait Hint: " << ssStatus.dwWaitHint << std::endl;
+    }
+    Cleanup(scm, serviceHandle);
+    return returnValue;
+}
